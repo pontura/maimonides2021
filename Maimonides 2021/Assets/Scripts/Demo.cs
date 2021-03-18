60b0c0a22c2a0375bedfabf8226f70e3ad60635a
Agustin Pontura <pontura@gmail.com>
2021-03-18 14:47:18 -0300

    demo 1

---
+++ Maimonides 2021/Assets/Scripts/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject ball;
+    public Vector3 cam_position; //  float, float, float
+
+    private void Update()
+    {
+        cam.transform.position = ball.transform.position;
+        cam.transform.localEulerAngles = ball.transform.localEulerAngles;
+    }
+}
