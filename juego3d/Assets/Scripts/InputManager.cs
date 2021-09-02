8964921ec775b612344620bd422dfac8dc9d76e6
Agustin Pontura <pontura@gmail.com>
2021-09-02 10:32:42 -0300

    viewController

--- juego3d/Assets/Scripts/InputManager.cs
+++ juego3d/Assets/Scripts/InputManager.cs
@@ -34,17 +34,16 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
             character.actions.Attack();
 
-        float _y = (Input.mousePosition.y) * filterY;
-        float _x = (Input.mousePosition.x - Screen.width / 2) * filterX;
+        //float _y = (Input.mousePosition.y) * filterY;
+        //float _x = (Input.mousePosition.x - Screen.width / 2) * filterX;
 
-        print(_y);
-        if (_y < limitsY.x) _y = limitsY.x;
-        else  if (_y > limitsY.y) _y = limitsY.y;
+        //if (_y < limitsY.x) _y = limitsY.x;
+        //else  if (_y > limitsY.y) _y = limitsY.y;
 
-        _y += offsetRotationY;
-        cameraPivot.transform.localEulerAngles = new Vector3(_y, 0,  0);
-        Vector3 rot = character.transform.localEulerAngles;
-        rot.y = _x;
-        character.transform.localEulerAngles = rot;
+        //_y += offsetRotationY;
+        //cameraPivot.transform.localEulerAngles = new Vector3(_y, 0,  0);
+        //Vector3 rot = character.transform.localEulerAngles;
+        //rot.y = _x;
+        //character.transform.localEulerAngles = rot;
     }
 }
